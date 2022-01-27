6be58e0a7cf90d77476e033081f3ff9142e0de2d
gahan k <gahan.kumar@konnectbox.in>
2022-01-27 18:29:57 +0530

    added all file

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'registration-form';
+  // isLoginClass="ABCD";
+
+
+// openRegister()
+// {
+//   this.isLoginClass="rotate";
+// }
+}
+
+
+
+
